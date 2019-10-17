48ab007ed13d8192ae09cd9d587c4c8a0ab5b642
Fabien Proriol <fabien.proriol@viavisolutions.com>
2019-10-17 14:20:45 +0200

    Build freertos as a static library

--- stm32l432kc.mk
+++ stm32l432kc.mk
@@ -1,6 +1,19 @@
-CFLAGS          += -I$(FREERTOS_DIR)/include 
-CPPFLAGS	+= -I$(FREERTOS_DIR)/include 
-CFLAGS          += -I$(FREERTOS_DIR)/portable/GCC/ARM_CM4F/ 
-CPPFLAGS	+= -I$(FREERTOS_DIR)/portable/GCC/ARM_CM4F/ 
-OBJS            += timer.o task.o list.o queue.o
-OBJS            += portable/GCC/ARM_CM4F/port.o
+CFLAGS          += -I$(FREERTOS_DIR)/include/
+CFLAGS          += -I$(FREERTOS_DIR)/portable/GCC/ARM_CM4F/
+FREEOBJS        += $(FREERTOS_DIR)/portable/GCC/ARM_CM4F/port.o
+FREEOBJS        += $(FREERTOS_DIR)/tasks.o
+FREEOBJS        += $(FREERTOS_DIR)/list.o
+FREEOBJS        += $(FREERTOS_DIR)/queue.o
+FREEOBJS        += $(FREERTOS_DIR)/timers.o
+
+#OBJS            += $(BUILD_DIR)/freertos/task.o $(BUILD_DIR)/freertos/list.o $(BUILD_DIR)/freertos/queue.o
+#OBJS            += $(BUILD_DIR)/freertos/portable/GCC/ARM_CM4F/port.o
+
+$(FREERTOS_DIR)/%.o: $(FREERTOS_DIR)/%.c
+	@printf "  CC\t$<\n"
+	@mkdir -p $(dir $@)
+	$(Q)$(CC) $(CFLAGS) $(ARCH_FLAGS) -o $@ -c $<
+
+
+$(FREERTOS_DIR)/libfreertos.a: $(FREEOBJS)
+	$(Q)$(AR) rc $(FREERTOS_DIR)/libfreertos.a $(FREEOBJS)
