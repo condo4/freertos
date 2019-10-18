a79f8a971b7f128c1a27dd98ffb9789f7c1f7da6
Fabien Proriol <fabien.proriol@viavisolutions.com>
2019-10-18 10:00:44 +0200

    Rename makefile script

--- freertos.mk
+++ freertos.mk
@@ -1,20 +1,16 @@
 CFLAGS          += -I$(FREERTOS_DIR)/include/
-CFLAGS          += -I$(FREERTOS_DIR)/portable/GCC/ARM_CM4F/
-FREEOBJS        += $(FREERTOS_DIR)/portable/GCC/ARM_CM4F/port.o
+CFLAGS          += -I$(FREERTOS_DIR)/portable/GCC/$(FREERTOS_PORT)/
+FREEOBJS        += $(FREERTOS_DIR)/portable/GCC/$(FREERTOS_PORT)/port.o
 FREEOBJS        += $(FREERTOS_DIR)/portable/MemMang/heap_$(FREERTOS_HEAP).o
 FREEOBJS        += $(FREERTOS_DIR)/tasks.o
 FREEOBJS        += $(FREERTOS_DIR)/list.o
 FREEOBJS        += $(FREERTOS_DIR)/queue.o
 FREEOBJS        += $(FREERTOS_DIR)/timers.o
 
-#OBJS            += $(BUILD_DIR)/freertos/task.o $(BUILD_DIR)/freertos/list.o $(BUILD_DIR)/freertos/queue.o
-#OBJS            += $(BUILD_DIR)/freertos/portable/GCC/ARM_CM4F/port.o
-
 $(FREERTOS_DIR)/%.o: $(FREERTOS_DIR)/%.c
 	@printf "  CC\t$<\n"
 	@mkdir -p $(dir $@)
 	$(Q)$(CC) $(CFLAGS) $(ARCH_FLAGS) -o $@ -c $<
-
 
 $(FREERTOS_DIR)/libfreertos.a: $(FREEOBJS)
 	$(Q)$(AR) rc $(FREERTOS_DIR)/libfreertos.a $(FREEOBJS)
