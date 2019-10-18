4fa75c38974b640629b903e4f10486a1ceef6313
Fabien Proriol <fabien.proriol@viavisolutions.com>
2019-10-18 14:20:20 +0200

    Add default value for FREERTOS_HEAP

--- freertos.mk
+++ freertos.mk
@@ -1,11 +1,14 @@
 CFLAGS          += -I$(FREERTOS_DIR)/include/
 CFLAGS          += -I$(FREERTOS_DIR)/portable/GCC/$(FREERTOS_PORT)/
+
 FREEOBJS        += $(FREERTOS_DIR)/portable/GCC/$(FREERTOS_PORT)/port.o
 FREEOBJS        += $(FREERTOS_DIR)/portable/MemMang/heap_$(FREERTOS_HEAP).o
 FREEOBJS        += $(FREERTOS_DIR)/tasks.o
 FREEOBJS        += $(FREERTOS_DIR)/list.o
 FREEOBJS        += $(FREERTOS_DIR)/queue.o
 FREEOBJS        += $(FREERTOS_DIR)/timers.o
+
+FREERTOS_HEAP   ?= 1
 
 $(FREERTOS_DIR)/%.o: $(FREERTOS_DIR)/%.c
 	@printf "  CC\t$<\n"
